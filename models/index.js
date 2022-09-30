49485e7ee3a736b09383c21497f302daebb698e5
Beanalini <dat826@gmail.com>
2022-09-30 15:51:09 +0100

    Coded routes for: get all products, get product by id  and product delete.

--- models/index.js
+++ models/index.js
@@ -6,26 +6,28 @@ const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
 Product.belongsTo(Category, {
-  foreignKey: category_id,
+  foreignKey: 'category_id',
 });
 
 // Categories have many Products
 Category.hasMany(Product, {
-  foreignKey: category_id
+  foreignKey: 'category_id',
 });
 
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag,{
   through:ProductTag,
-  foreignKey:ProductId,
+  foreignKey:'product_id',
+  unique: false
 });
 
 
 // Tags belongToMany Products (through ProductTag)
 
-Tag.belongsToMany(Products, {
+Tag.belongsToMany(Product, {
   through: ProductTag,
-  ForeignKey: tag_id
+  ForeignKey: 'tag_id',
+  unique:false
 })
 
 module.exports = {
